1fda54cf3901aa5cb868e24284310880472d7634
Andriy Yavorskiy <aiavorskii@gmail.com>
2022-07-13 01:59:34 +0300

    fixing install script

--- scripts/install-local.sh
+++ scripts/install-local.sh
@@ -4,4 +4,5 @@ docker exec -t feellucky_local_app composer install && \
 docker exec -t feellucky_local_app php artisan key:generate && \
 docker exec -t feellucky_local_app chown 1000:1000 -R /var/www/ && \
 docker exec -t feellucky_local_app chown 1000:www-data -R /var/www/storage && \
-docker exec -t feellucky_local_app chmod 775 -R /var/www/storage
+docker exec -t feellucky_local_app chmod 775 -R /var/www/storage && \
+docker-compose -f docker-compose-local.yml up -d
