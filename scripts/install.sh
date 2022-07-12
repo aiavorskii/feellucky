d3f359a9ea7d1774df10597e3590fb340c229dcb
Andriy Yavorskiy <aiavorskii@gmail.com>
2022-07-13 02:55:05 +0300

    fixing docker-compose and env files

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,4 +10,5 @@ docker exec -t feellucky_app php artisan key:generate && \
 docker exec -t feellucky_app chown 1000:1000 -R /var/www/ && \
 docker exec -t feellucky_app chown 1000:www-data -R /var/www/storage && \
 docker exec -t feellucky_app chmod 775 -R /var/www/storage && \
+docker exec -t feellucky_app npm i && \
 docker-compose -f docker-compose.yml up -d 
