36d18484ca98f4fbdf5a8fb3b79787a3459f2604
Andriy Yavorskiy <aiavorskii@gmail.com>
2022-07-13 01:48:24 +0300

    fixing install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
-
+cp ./app/.env.example ./app/.env && \
 docker-compose -f docker-compose.yml up -d && \
 docker exec -t feellucky_app composer install && \
+docker exec -t feellucky_app php artisan key:generate && \
 docker exec -t feellucky_app chown 1000:1000 -R /var/www/ && \
 docker exec -t feellucky_app chown 1000:www-data -R /var/www/storage && \
 docker exec -t feellucky_app chmod 775 -R /var/www/storage
